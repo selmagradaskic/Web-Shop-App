f0b81df77b810d7783b6d5f5e1736dd80dad9728
selmagradaskic <selma952010@gmail.com>
2023-08-22 04:21:57 +0200

    Making Responsive Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [{ path: '', component: HomePageComponent },
-{ path: 'reviews-component', component: ReviewsComponent }
+{ path: 'reviews', component: ReviewsComponent }
 ]; 
 
 @NgModule({
